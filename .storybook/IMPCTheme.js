2ab73b9d2dc559452fbbe322e1a8ae6e2e4cf4c5
Federico Lopez Gomez <federico@ebi.ac.uk>
2021-01-26 11:33:21 +0000

    Adding IMPC branding and theme to StoryBookJS

---
+++ .storybook/IMPCTheme.js
@@ -0,0 +1,12 @@
+// .storybook/IMPCTheme.js
+
+import { create } from '@storybook/theming/create';
+
+export default create({
+    base: 'light',
+    brandTitle: 'International Mouse Phenotyping Consortium',
+    brandUrl: 'https://www.mousephenotype.org',
+    brandImage: 'https://www.mousephenotype.org/wp-content/themes/impc/images/IMPC_logo.svg',
+    colorPrimary: '#ed7b25',
+    colorSecondary: '#00b0b0',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,8 @@
+// .storybook/manager.js
+
+import { addons } from '@storybook/addons';
+import IMPCTheme from './IMPCTheme';
+
+addons.setConfig({
+    theme: IMPCTheme,
+});
